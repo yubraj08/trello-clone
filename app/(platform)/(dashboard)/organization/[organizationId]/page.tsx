4029574145efc5195a26e52fb1457ecd62c795c8
yubraj08 <yubrajadhikari2019@gmail.com>
2024-03-09 07:03:27 +0545

    schema of database added

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,23 +1,23 @@
 import { Suspense } from "react";
 
-// import { Separator } from "@/components/ui/separator";
+import { Separator } from "@/components/ui/separator";
 
-// import { Info } from "./_components/info";
-// import { BoardList } from "./_components/board-list";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
 // import { checkSubscription } from "@/lib/subscription";
 
 const OrganizationIdPage = async () => {
-//   const isPro = await checkSubscription();
+  // const isPro = await checkSubscription();
 
   return (
     <div className="w-full mb-20">
-      {/* <Info isPro={isPro} />
+      <Info isPro={isPro} />
       <Separator className="my-4" />
       <div className="px-2 md:px-4">
-        <Suspense fallback={<BoardList.Skeleton />}>
+        {/* <Suspense fallback={<BoardList.Skeleton />}>
           <BoardList />
-        </Suspense>
-      </div> */}
+        </Suspense> */}
+      </div>
       Organization
     </div>
   );
